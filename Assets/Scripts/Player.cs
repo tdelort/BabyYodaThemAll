9937caaeeeffe5abca9200d6106e471b2d814716
tdelort <tristan.delort@gmail.com>
2021-11-24 19:56:19 -0500

    Added map and movment works (server authority now)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,39 @@ public class Player : NetworkBehaviour
 {
     [SerializeField] private float speed = 5f;
 
+    public override void OnNetworkSpawn()
+    {
+        Debug.Log("Player started");
+        if (IsLocalPlayer)
+        {
+            var camera = Camera.main;
+            camera.transform.position = transform.position + new Vector3(0, 10, -10);
+            camera.transform.rotation = Quaternion.Euler(45, 0, 0);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(IsLocalPlayer)
         {
+            // gathering inputs
             Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             Vector3 movement = input * speed * Time.deltaTime;
-            Debug.Log("Input : " + input);
-            transform.Translate(movement);
+            MovePlayerServerRpc(movement);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Action1();
+            }
+
         }
     }
 
-    //[ServerRpc]
-    //public void MovePlayerServerRpc(Vector3 movement)
-    //{
-    //    Debug.Log("MovePlayerServerRpc : " + movement);
-    //    transform.position += movement;
-    //}
+    [ServerRpc]
+    public void MovePlayerServerRpc(Vector3 movement)
+    {
+        Debug.Log("MovePlayerServerRpc : " + movement);
+        transform.position += movement;
+    }
 }
